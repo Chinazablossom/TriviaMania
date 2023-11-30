4a9657cf94b96536f2d00be0ce60aaf62cc48480
Chinaza Blossom <chinazablossom7@gmail.com>
2023-11-30 11:51:15 +0100

    seventh commit

--- app/src/main/java/com/example/triviamania/ui/MainActivity.kt
+++ app/src/main/java/com/example/triviamania/ui/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.triviamania.ui
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.activityViewModels
 import com.example.triviamania.R
 import com.example.triviamania.viewModels.GameSoundViewModel
 import com.example.triviamania.viewModels.SoundViewModel
@@ -16,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
 
     }
 
